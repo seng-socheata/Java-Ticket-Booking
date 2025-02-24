93794aef26a346673448dd2e0df43714b10d2c55
tongeangthet <thettongeang@gmail.com>
2025-02-24 16:56:40 +0700

    Admin updated

--- src/AdminManage/AdminSignUpLogin.java
+++ src/AdminManage/AdminSignUpLogin.java
@@ -4,26 +4,14 @@ package AdminManage;
 class Admin {
     private String username;
     private String password;
-    private String email;
-    private String phoneNumber;
 
-    Admin(String username, String email, String phoneNumber, String password) {
+    Admin(String username, String password) {
         this.username = username;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
         this.password = password;
     }
 
     public String getUsername() {
         return username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
     }
 
     public String getPassword() {
